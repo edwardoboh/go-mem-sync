84b41735740d15c5db51ebb9a59e08d9f25ee6e6
edwardoboh <obohedward@gmail.com>
2023-05-28 16:13:07 +0100

    feat: add synchronization using a mutex that locks read and write access

--- sync.go
+++ sync.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"sync"
 	"time"
 )
 
 // This hold the state of the player
 type Player struct {
+	mu     sync.RWMutex
 	health int
 }
 
@@ -23,7 +25,14 @@ func StartGameUI(player *Player) {
 	ticker := time.NewTicker(time.Second)
 	for {
 		os.Stdout.Sync()
+
+		// (Read) Lock the struct to ensure no routine has access to change it's value
+		player.mu.RLock()
+
 		fmt.Printf("Player health: %v\t\r", player.health)
+
+		player.mu.RUnlock()
+
 		<-ticker.C
 	}
 }
@@ -31,11 +40,17 @@ func StartGameUI(player *Player) {
 func StartGameLoop(player *Player) {
 	for {
 		timer := time.NewTimer(time.Millisecond * 600)
+
+		// (Write) Lock the struct before modifying it's value to ensure no routine is currently reading
+		player.mu.Lock()
+
 		player.health -= rand.Intn(20)
 		if player.health <= 0 {
 			fmt.Println("Game Over")
 			return
 		}
+
+		player.mu.Unlock()
 		<-timer.C
 	}
 }
